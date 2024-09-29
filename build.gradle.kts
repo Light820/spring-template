7db2da6ffb893a8c95c4f2aee317b1c3202fd277
Light <light820@proton.me>
2024-09-29 04:47:52 +0200

    ADD missing apply false to plugin declaration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@ plugins {
     alias(libs.plugins.spring.boot) apply false
     alias(libs.plugins.spring.dependency.management) apply false
     alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.kotlin.spring) apply false
 }
